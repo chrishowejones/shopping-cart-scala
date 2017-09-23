bcaad546bceae41db7a828f283742ded07ac58f9
Chris Howe-Jones <chris.howejones@gmail.com>
2017-09-23 18:03:40 +0100

    Use Try on priceMatrix lookups

--- src/main/scala/example/ShoppingCart.scala
+++ src/main/scala/example/ShoppingCart.scala
@@ -1,5 +1,7 @@
 package example
 
+import scala.util.Try
+
 class ShoppingCart(priceMatrix: Map[String, () => Double]) {
 
   def checkout(items: List[String]): Double = {
@@ -16,6 +18,19 @@ class ShoppingCart(priceMatrix: Map[String, () => Double]) {
         price(total, item))
   }
 
+  def checkoutWithTry(items: List[String]): Double = {
+    def price(total: Double, item: String) = {
+      val priceCalc = Try(priceMatrix(item))
+        .getOrElse { throw new IllegalStateException("Illegal item added") }
+      total + priceCalc()
+    }
+
+    items.filter(_ != null)
+      .map(_.toLowerCase)
+      .foldLeft(0.0) ((total, item) =>
+      price(total, item))
+  }
+
 }
 
 object ShoppingCart {
@@ -27,4 +42,5 @@ object ShoppingCart {
 
   def checkout(items: List[String]): Double = new ShoppingCart(defaultPriceMatrix).checkout(items)
 
+  def checkoutWithTry(items: List[String]): Double = new ShoppingCart(defaultPriceMatrix).checkout(items)
 }
--- src/test/scala/example/ShoppingCartSpec.scala
+++ src/test/scala/example/ShoppingCartSpec.scala
@@ -44,4 +44,49 @@ class ShoppingCartSpec extends FlatSpec with Matchers {
   it should "have a value of 0 pence when a nil is checked out" in {
     ShoppingCart.checkout(Nil) shouldEqual 0.0
   }
+
+  // same tests for checkoutWithTry
+
+    "A ShoppingCart checkoutWithTry" should "have a value of 60 pence when it contains an apple" in {
+    ShoppingCart.checkoutWithTry(List("apple")) shouldEqual 0.60
+  }
+
+  it should "have a value of 25 pence when it contains an orange" in {
+    ShoppingCart.checkoutWithTry(List("orange")) shouldEqual 0.25
+  }
+
+  it should "have a value of 120 pence when it contains two apples" in {
+    ShoppingCart.checkoutWithTry("apple" :: "apple" :: Nil) shouldEqual 1.20
+  }
+
+  it should "have a value of 50 pence when it contains two oranges" in {
+    ShoppingCart.checkoutWithTry("orange" :: "orange" :: Nil) shouldEqual 0.50
+  }
+
+  it should "have a value of 205 pence when it contains 3 apples and an orange" in {
+    ShoppingCart.checkoutWithTry("apple" :: "apple" :: "orange" :: "apple" :: Nil) shouldEqual 2.05
+  }
+
+  it should "have a value of 0 pence when it contains null" in {
+    ShoppingCart.checkoutWithTry(List(null, null)) shouldEqual 0.0
+  }
+
+  it should "have a value of 60 pence when it contains apple and nulls" in {
+    ShoppingCart.checkoutWithTry(List(null, "apple", null)) shouldEqual 0.60
+  }
+
+  it should "throw an illegal state exception when it contains grapes" in {
+    the[IllegalStateException] thrownBy
+      ShoppingCart.checkoutWithTry(List("grapes")) should have message "Illegal item added"
+  }
+
+  it should "have a value of 0 pence when an empty list is checked out" in {
+    ShoppingCart.checkoutWithTry(List()) shouldEqual 0.0
+  }
+
+  it should "have a value of 0 pence when a nil is checked out" in {
+    ShoppingCart.checkoutWithTry(Nil) shouldEqual 0.0
+  }
+
+
 }
